005373a3acf5b5dfbc36542443732513d1724124
Laksara K.Y. <129388843+YasiruLaksara@users.noreply.github.com>
2025-06-12 17:20:23 +0530

    Add InnovatorProfile route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { KeycloakProvider } from './context/KeycloakContext';
 import Home from './pages/Home';
+import InnovatorProfile from './pages/InnovatorProfile';
 
 function App() {
   return (
     <KeycloakProvider>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/innovator-profile" element={<InnovatorProfile />} />
+        </Routes>
+      </Router>
     </KeycloakProvider>
   );
 }
