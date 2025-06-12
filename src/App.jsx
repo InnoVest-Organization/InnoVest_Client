9b59baaff771ff65405bf0eb86e9c5c72d6c1ce7
Laksara K.Y. <129388843+YasiruLaksara@users.noreply.github.com>
2025-06-13 03:09:03 +0530

    added routing for InnovatorProfile and InnovationRegistration components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { KeycloakProvider } from './context/KeycloakContext';
 import Home from './pages/Home';
 import InnovatorProfile from './pages/InnovatorProfile';
+import InnovationRegistration from './pages/InnovationRegistration';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/innovator-profile" element={<InnovatorProfile />} />
+          <Route path="/innovation-registration" element={<InnovationRegistration />} />
         </Routes>
       </Router>
     </KeycloakProvider>
