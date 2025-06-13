847cd134513c81aaa600880bf005a3a948bf66a3
RanaweeraHK <ranaweerahk.21@uom.lk>
2025-06-13 13:02:33 +0530

    Export UI components for SuccessStory

---
+++ src/components/SuccessStory/ui/index.js
@@ -0,0 +1,5 @@
+// Export all UI components for SuccessStory
+export { Button } from './Button';
+export { Input } from './Input';
+export { Label } from './Label';
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card';
